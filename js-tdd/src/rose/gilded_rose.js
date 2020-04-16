05a0c504bcdeb1d5ef039cda3771f6e41b0432ab
Anthony <anthony.ringard@accenture.com>
2020-04-16 15:45:15 +0200

    add refacto 1

--- js-tdd/src/rose/gilded_rose.js
+++ js-tdd/src/rose/gilded_rose.js
@@ -19,6 +19,7 @@ class Shop {
                 item.quality = item.quality + 1
             }
         }
+
         function decreaseQuality(item) {
             if (item.quality > 0) {
                 if (item.name != 'Sulfuras, Hand of Ragnaros') {
@@ -26,12 +27,16 @@ class Shop {
                 }
             }
         }
+        const nameException = ['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert'];
+
         for (var i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                decreaseQuality(this.items[i])
-            } else {
+
+
+            if (true === nameException.includes(this.items[i].name)) {
+
                 if (this.items[i].quality < 50) {
                     this.items[i].quality = this.items[i].quality + 1
+
                     if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
                         if (this.items[i].sellIn < 11) {
                             increaseQualityUntilMax(this.items[i])
@@ -41,19 +46,23 @@ class Shop {
                         }
                     }
                 }
+            } else {
+                decreaseQuality(this.items[i])
             }
+
             if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
                 this.items[i].sellIn = this.items[i].sellIn - 1
             }
             if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Aged Brie') {
-                    if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        decreaseQuality(this.items[i])
-                    } else {
-                        this.items[i].quality = 0
-                    }
-                } else {
+
+                if (this.items[i].name == 'Aged Brie') {
                     increaseQualityUntilMax(this.items[i])
+                } else {
+                    if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert') {
+                        this.items[i].quality = 0
+                    } else {
+                        decreaseQuality(this.items[i])
+                    }
                 }
             }
         }

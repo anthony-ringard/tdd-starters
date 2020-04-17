7955620061579733c888ea0a7632a842fc9200fd
Anthony <anthony.ringard@accenture.com>
2020-04-17 16:02:49 +0200

    init tests trivia

--- js-tdd/src/trivia/spec/game.test.js
+++ js-tdd/src/trivia/spec/game.test.js
@@ -10,4 +10,23 @@ describe('The test environment', () => {
   it('should access game', () => {
     expect(Game).toBeDefined();
   })
+
+  it('should add players and display their name and number', ()=>{
+    var game = new Game()
+    var consoleValues = [];
+    var logMock = function(value) {
+        consoleValues.push(value)
+    }
+    console.log = logMock
+
+    expect(game.add('Julien')).toBe(true)
+    expect(game.add('Marvin')).toBe(true)
+    expect(consoleValues).toEqual([
+      'Julien was added',
+      "They are player number 1",
+      'Marvin was added',
+      "They are player number 2",
+    ])
+
+  })
 })

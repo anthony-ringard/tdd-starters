611a4201f612b63eb832a5f4a76a4cd352f472dc
Anthony <anthony.ringard@accenture.com>
2020-04-29 15:26:37 +0200

    add kata parrot refacto

---
+++ js-tdd/src/Parrot-Refactoring-Kata/parrot/parrot.js
@@ -0,0 +1,40 @@
+class Parrot {
+    constructor(numberOfCoconuts, voltage, isNailed) {
+        this.numberOfCoconuts = numberOfCoconuts;
+        this.voltage = voltage;
+        this.isNailed = isNailed;
+    }
+
+    getBaseSpeedWithVoltage(voltage) {
+        return Math.min(24, voltage * this.getBaseSpeed());
+    }
+
+    getLoadFactor() {
+        return 9;
+    }
+
+    getBaseSpeed() {
+        return 12;
+    }
+}
+
+export class European extends Parrot {
+    getSpeed() {
+        return super.getBaseSpeed();
+    }
+}
+
+export class African extends Parrot {
+
+    getSpeed() {
+        return Math.max(0, super.getBaseSpeed() - super.getLoadFactor() * this.numberOfCoconuts);
+    }
+}
+
+
+export class NorwegianBlue extends Parrot {
+
+    getSpeed() {
+        return (this.isNailed) ? 0 : super.getBaseSpeedWithVoltage(this.voltage);
+    }
+}
